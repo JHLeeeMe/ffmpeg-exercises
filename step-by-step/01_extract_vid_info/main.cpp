17d3cdf5399b562902d0ef9301ba105c49a12725
JHLeeeMe <lejung92@gmail.com>
2022-12-05 10:17:34 +0900

    [Update] ```01_extract_vid_info``` 완성
    	수정함:        step-by-step/01_extract_vid_info/main.cpp

--- step-by-step/01_extract_vid_info/main.cpp
+++ step-by-step/01_extract_vid_info/main.cpp
@@ -7,13 +7,27 @@ extern "C"
 
 int main()
 {
-    std::cout << "hello, FFmpeg" << std::endl;
+    const std::string root_path = "../../";
+    const std::string file_path = root_path + "resources/media/videos/night_sky.mp4";
+
     AVFormatContext* in_fmt_ctx = avformat_alloc_context();
-    if (avformat_open_input(&in_fmt_ctx, "", nullptr, nullptr) < 0)
+    if (avformat_open_input(&in_fmt_ctx, file_path.c_str(), nullptr, nullptr) < 0)
     {
         std::cerr << "avformat_open_input(...) failed..." << std::endl;
         return -1;
     }
+
+    if (avformat_find_stream_info(in_fmt_ctx, nullptr) < 0)
+    {
+        std::cerr << "avformat_find_stream_info(...) failed..." << std::endl;
+        return -1;
+    }
+
+    std::cout << "numbers of streams: " << in_fmt_ctx->nb_streams    << std::endl;
+    std::cout << "duration: " << in_fmt_ctx->duration / AV_TIME_BASE << "s" << std::endl;
+    std::cout << "bitrate: " << in_fmt_ctx->bit_rate                 << std::endl;
+
+    avformat_close_input(&in_fmt_ctx);
 
     return 0;
 }
