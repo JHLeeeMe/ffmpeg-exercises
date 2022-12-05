ab0328bc475a202d521a86b477c69691a1cd7324
JHLeeeMe <lejung92@gmail.com>
2022-12-05 11:22:05 +0900

    [Update] deep dive streams
    	수정함:        step-by-step/02_streams/main.cpp

--- step-by-step/02_streams/main.cpp
+++ step-by-step/02_streams/main.cpp
@@ -23,25 +23,29 @@ int main()
         return -1;
     }
 
-    for (int i = 0; i < in_fmt_ctx->nb_streams; i++)
-    {
-        AVStream* stream = in_fmt_ctx->streams[i];
+    AVCodec*  video_codec;
+    AVCodec*  audio_codec;
+    int       video_stream_idx = av_find_best_stream(in_fmt_ctx, AVMEDIA_TYPE_VIDEO, -1, -1, &video_codec, 0);
+    int       audio_stream_idx = av_find_best_stream(in_fmt_ctx, AVMEDIA_TYPE_AUDIO, -1, video_stream_idx, &audio_codec, 0);
+    AVStream* video_stream = in_fmt_ctx->streams[video_stream_idx];
+    AVStream* audio_stream = in_fmt_ctx->streams[audio_stream_idx];
+    AVCodecParameters* video_codec_params = video_stream->codecpar;
+    AVCodecParameters* audio_codec_params = audio_stream->codecpar;
 
-        //int vid_idx = av_find_best_stream(in_fmt_ctx, AVMEDIA_TYPE_VIDEO, -1, -1, nullptr, 0);
-        //std::cout << vid_idx << std::endl;
-        switch (stream->codecpar->codec_type)
-        {
-        case AVMEDIA_TYPE_VIDEO:
-            std::cout << "video stream index: " << i << std::endl;
-            av_dump_format(in_fmt_ctx, i, file_path.c_str(), 0);  // print details
-            continue;
-        case AVMEDIA_TYPE_AUDIO:
-            std::cout << "audio stream index: " << i << std::endl;
-            continue;
-        default:
-            continue;
-        }
-    }
+    std::cout << "number of frames: " << video_stream->nb_frames << std::endl
+              << "framerate: " << video_stream->avg_frame_rate.num << "/" << video_stream->avg_frame_rate.den << std::endl
+              << "time base: " << video_stream->time_base.num << "/" << video_stream->time_base.den << std::endl
+              << "resolution: " << video_stream->codecpar->width << "x" << video_stream->codecpar->height << std::endl
+              << "color format: " << video_stream->codecpar->format << std::endl
+              << "codec(id): " << video_codec->name << "(" << video_codec_params->codec_id << ")" << std::endl
+              << "------------------------------------------------------" << std::endl
+              << "number of frames: " << audio_stream->nb_frames << std::endl
+              << "time base: " << audio_stream->time_base.num << "/" << audio_stream->time_base.den << std::endl
+              << "sound format: " << audio_stream->codecpar->format << std::endl
+              << "channels: " << audio_codec_params->channels << std::endl
+              << "sample rate: " << audio_codec_params->sample_rate << std::endl
+              << "codec(id): " << audio_codec->name << "(" << audio_codec_params->codec_id << ")" << std::endl
+    << std::endl;
 
     avformat_close_input(&in_fmt_ctx);
 
